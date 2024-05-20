f64faca6ac966f808ded2d66ebea428770e42438
Clyster <14972204+Clyster@users.noreply.github.com>
2024-05-20 22:45:20 +0800

    Add files via upload

--- PE_FL/criteria.py
+++ PE_FL/criteria.py
@@ -27,3 +27,31 @@ class MaskedL1Loss(nn.Module):
         diff = diff[valid_mask]
         self.loss = diff.abs().mean()
         return self.loss
+    
+
+class HuberLoss(nn.Module):
+    def __init__(self, delta=17):  # 290开根号
+        super(HuberLoss, self).__init__()
+        self.delta = delta
+
+    def forward(self, pred, target):
+        assert pred.dim() == target.dim(), "inconsistent dimensions"
+        error = target - pred
+        valid_mask = (target > 0).detach()
+        error = error[valid_mask]
+        is_small_error = torch.abs(error) <= self.delta
+        small_error_loss = 0.5 * error**2
+        large_error_loss = self.delta * (torch.abs(error) - 0.5 * self.delta)
+        loss = torch.where(is_small_error, small_error_loss, large_error_loss)
+        return loss.mean()
+    
+
+class LogCoshLoss(nn.Module):
+    def __init__(self):
+        super(LogCoshLoss, self).__init__()
+
+    def forward(self, pred, target):
+        assert pred.dim() == target.dim(), "inconsistent dimensions"
+        diff = pred - target
+        loss = torch.log(torch.cosh(diff))
+        return loss.mean()
